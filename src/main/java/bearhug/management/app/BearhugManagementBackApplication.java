e22f7c29557cb5e4f18e5638a3ce635773e390ad
Jose Alberto Cruz Martinez <albertocruz8133@proton.me>
2024-12-14 19:21:22 -0600

    init project

---
+++ src/main/java/bearhug/management/app/BearhugManagementBackApplication.java
@@ -0,0 +1,13 @@
+package bearhug.management.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BearhugManagementBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BearhugManagementBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bearhug/management/app/BearhugManagementBackApplicationTests.java
@@ -0,0 +1,13 @@
+package bearhug.management.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BearhugManagementBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
